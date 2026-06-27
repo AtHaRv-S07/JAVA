bab2c6c78bc2afa89b2c52a6cebcb15d7b6bf816
Atharva sitoke <sitokeatharv33@gmail.com>
2026-06-28 00:58:15 +0530

    ✅ Java Substring Comparisons | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-06-27

---
+++ hackerrank/medium/java-string-reverse/solution.cpp
@@ -0,0 +1,26 @@
+
+    public static String getSmallestAndLargest(String s, int k) {
+        String smallest = s.substring(0,k);
+        String largest = s.substring(0,k);
+        int l=s.length();
+        for(int i=1;i<s.length()-k+1;i++)
+        {
+            String current=s.substring(i,i+k);
+            
+            if(current.compareTo(smallest)<0){
+                smallest=current;
+            }
+            
+            if(current.compareTo(largest)>0){
+                largest=current;
+            }
+        }
+        
+        // Complete the function
+        // 'smallest' must be the lexicographically smallest substring of length 'k'
+        // 'largest' must be the lexicographically largest substring of length 'k'
+        
+        return smallest + "\n" + largest;
+    }
+
+
